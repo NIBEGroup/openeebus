08c49418bbce07630771ca29e97b6e91b7677922
Gennadii Voitov <gennadii.voitov@sigma.software>
2026-09-16 17:38:59 +0300

    In `ShipNode::SkiMatches()` replace `strcmp()` with `strcasecmp()`

--- src/ship/ship_node/ship_node.c
+++ src/ship/ship_node/ship_node.c
@@ -409,7 +409,7 @@ bool SkiMatches(const char* ski_a, const char* ski_b) {
     return false;
   }
 
-  return strcmp(ski_a, ski_b) == 0;
+  return strcasecmp(ski_a, ski_b) == 0;
 }
 
 static bool ShipNodeFindServiceForSki(ShipNode* self, const char* ski, MdnsEntry* found_entry) {
